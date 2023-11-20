1ae625bf2d975fa6584d8c4ef9c19de8180c835a
iamraghavan <raghavanofficials@gmail.com>
2023-11-20 19:45:47 +0530

    update firebase

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,13 @@ import { updateProfile, sendEmailVerification } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
     apiKey: "AIzaSyBtbZTgxZ-0VYsdEfIvlkHwmk5Ak4d2XyM",
-  authDomain: "kurudhi-6135d.firebaseapp.com",
-  projectId: "kurudhi-6135d",
-  storageBucket: "kurudhi-6135d.appspot.com",
-  messagingSenderId: "78326000398",
-  appId: "1:78326000398:web:1953cb2b173b882d87455f",
-  measurementId: "G-3VRSYK6VLF"
+    authDomain: "kurudhi-6135d.firebaseapp.com",
+    databaseURL: "https://kurudhi-6135d-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "kurudhi-6135d",
+    storageBucket: "kurudhi-6135d.appspot.com",
+    messagingSenderId: "78326000398",
+    appId: "1:78326000398:web:1953cb2b173b882d87455f",
+    measurementId: "G-3VRSYK6VLF"
 };
 
 // Initialize Firebase
